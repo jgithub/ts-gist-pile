d5e597454cb53fdc7eb5d0dfee114879d17c12b1
jgithub <jgithub1@jeano.net>
2023-10-31 03:27:54 -0500

    dev

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 export * as stringUtil from "./string/stringUtil"
+export * as booleanUtil from "./boolean/booleanUtil"
+export * as logUtil from "./log/logUtil"
+export { getLogger } from "./log/getLogger"
+export { d4l } from "./log/logUtil"
+
 export { DateProviderServiceImpl } from "./date/DateProviderServiceImpl"
 export { DateProviderService } from "./date/DateProviderService"
-export * as booleanUtil from "./boolean/booleanUtil"
