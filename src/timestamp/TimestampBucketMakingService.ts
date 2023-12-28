d440dd6144b361dc6f7bde0159ff19322f350333
jgithub <jgithub1@jeano.net>
2023-12-28 13:26:44 -0600

    tests

--- src/timestamp/TimestampBucketMakingService.ts
+++
@@ -1,8 +0,0 @@
-export interface TimestampBucketMakingService {
-  /**
-   * Return value is a sorted array of bucket starting times since epoch in Seconds
-   * @param year 
-   */
-  gengetSorted5MinuteBucketStartingUnixtimesForYearInSeconds(year: number): ReadonlyArray<number>
-  getMyCurrent5MinuteBucketStartTimeSecondsSinceEpoch(): number
-}
