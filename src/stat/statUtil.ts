c7289b07a1f2543cbe94108e3a273702ecbbbca6
jgithub <jgithub1@jeano.net>
2023-12-31 09:26:00 -0600

    0.0.23; statUtil

---
+++ src/stat/statUtil.ts
@@ -0,0 +1,25 @@
+export function sendStatToKpitracks(requestBodyString: string): void {
+  if (process.env.KPITRACKS_EZ_KEY != null && process.env.KPITRACKS_EZ_KEY.trim()?.length > 0 && process.env.KPITRACKS_ERROR_KEY != null && process.env.KPITRACKS_ERROR_KEY.trim()?.length > 0) {
+    const controller = new AbortController()
+    const timeoutId = setTimeout(() => controller.abort(), 750)
+    const url = "https://stat.kpitracks.com/c"
+    requestBodyString = requestBodyString + "&ezkey=${process.env.KPITRACK_EZ_KEY?.trim()}"
+
+    // console.log(`Sending POST to Stathat url = '${url}',  requestBody = '${requestBody}'`)
+
+    const beforeAt = new Date()
+    // While still experimental, the global fetch API is available by default in Node.js 18
+    fetch(url, { 
+      method: 'POST', 
+      signal: controller.signal,
+      body: requestBodyString
+    }).then(response => {
+      // completed request before timeout fired
+      const deltaInMs = new Date().getTime() - beforeAt.getTime()
+      // console.log(`Stathat fetch completed after ${deltaInMs} milliseconds,  with response = ${d4l(response)}`)
+
+      // If you only wanted to timeout the request, not the response, add:
+      clearTimeout(timeoutId)
+    }).catch(err => {
+    })
+}
