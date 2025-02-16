f9070afabf36a096d15852c5c2433784fd46d35a
JV <jgithub@users.noreply.github.com>
2025-02-16 14:22:47 -0600

    ensureNumber

--- src/number/numberUtil.ts
+++ src/number/numberUtil.ts
@@ -1,9 +1,12 @@
-export function tryAsNumber( input: unknown ): undefined | number {
+export function tryAsNumber( input: string | number | null | undefined ): undefined | number | null {
   if (typeof input == 'number') {
     return input
   }
-  else if (input == null) {
+  else if (typeof input == 'undefined') {
     return undefined
+  }
+  else if (input == null) {
+    return null
   }
   else if (typeof input == 'string') {
     const workingInput = input.trim()
@@ -16,4 +19,29 @@ export function tryAsNumber( input: unknown ): undefined | number {
     }
   } 
   return undefined
+}
+
+/**
+ * Converts the input into a number if it can.  Otherwise, throws an error.
+ * 
+ * @param input 
+ * @returns 
+ */
+
+export function ensureNumber( input: string | number ): number {
+  if (typeof input == 'number') {
+    return input
+  }
+  else if (typeof input == 'string') {
+    const workingInput = input.trim()
+    if (workingInput.length > 0 && 
+        !isNaN(workingInput as unknown as number)) {
+      try {
+        return parseFloat(input)
+      } catch (err) {
+        throw err
+      }
+    }
+  }
+  throw new Error(`Not a number`)
 }
