292f49513675ba89e640867e3d19c48756e9b7ac
JV <jgithub@users.noreply.github.com>
2025-06-24 17:40:10 -0500

    0.0.211; better env var for VITE_ and REACT_

--- src/env/internalEnvHelper.ts
+++ src/env/internalEnvHelper.ts
@@ -1,19 +1,45 @@
 export function tryGetEnvVar(envVarName: string): string | undefined {
   // @ts-ignore: import.meta may not be recognized depending on tsconfig/module system
-  try {
-    // Avoid TS1343 by putting this inside a dynamically eval'd function
-    const importMeta = new Function('return import.meta')();
-    if (typeof importMeta !== 'undefined' && typeof importMeta?.env?.[envVarName] !== 'undefined') {
-      // @ts-ignore: TypeScript doesn't know about Vite's import.meta.env
-      return importMeta.env[envVarName];
+  let retval: string | undefined = undefined;
+
+  if (typeof retval === 'undefined') {
+    try {
+      // Avoid TS1343 by putting this inside a dynamically eval'd function
+      const importMeta = new Function('return import.meta')();
+      if (typeof importMeta !== 'undefined' && typeof importMeta?.env?.[envVarName] !== 'undefined') {
+        // @ts-ignore: TypeScript doesn't know about Vite's import.meta.env
+        retval = importMeta.env[envVarName];
+      }
+    }
+    catch {
+      // Safe to ignore — import.meta not available
     }
   }
-  catch {
-    // Safe to ignore — import.meta not available
+
+  if (typeof window !== "undefined" && typeof retval === 'undefined') {
+    try {
+      // Avoid TS1343 by putting this inside a dynamically eval'd function
+      const importMeta = new Function('return import.meta')();
+      if (typeof importMeta !== 'undefined' && typeof importMeta?.env?.[envVarName] !== 'undefined') {
+        // @ts-ignore: TypeScript doesn't know about Vite's import.meta.env
+        retval = importMeta.env[`VITE_` + envVarName];
+      }
+    }
+    catch {
+      // Safe to ignore — import.meta not available
+    }
+  }  
+
+  if (typeof retval === 'undefined') {
+    if (typeof process !== 'undefined' && typeof process.env?.[envVarName] !== 'undefined') {
+      return process.env[envVarName];
+    }
   }
 
-  if (typeof process !== 'undefined' && typeof process.env?.[envVarName] !== 'undefined') {
-    return process.env[envVarName];
+  if (typeof window !== "undefined" && typeof retval === 'undefined') {
+    if (typeof process !== 'undefined' && typeof process.env?.[envVarName] !== 'undefined') {
+      return process.env[`REACT_` + envVarName];
+    }
   }
 
   return undefined;
