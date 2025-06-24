739d522d99706c7cc0555838a484b40551bd155f
JV <jgithub@users.noreply.github.com>
2025-06-24 13:43:31 -0500

    convertHexToNanoIdAlphabet

--- test/id/idUtil.test.ts
+++ test/id/idUtil.test.ts
@@ -7,15 +7,22 @@ describe('idUtil', () => {
       it('does something positive', () => {
         const generatedId = idUtil.generateTimeOrderedBase62Id()
         const anotherGeneratedId = idUtil.generateTimeOrderedBase62Id()
+        const thirdNanoIdLike = idUtil.generateTimeOrderedNanoLikeId()
 
         console.log(`generatedId = ${generatedId}`)
         console.log(`anotherGeneratedId = ${anotherGeneratedId}`)
+        console.log(`thirdNanoIdLike = ${thirdNanoIdLike}`)
 
         expect(generatedId).not.to.be.null;
         expect(generatedId.length).to.be.eq(21);
         expect(generatedId.startsWith('3')).to.be.true
+        expect(anotherGeneratedId.startsWith('3')).to.be.true
+
         expect(generatedId.startsWith('30')).to.be.true
+        expect(anotherGeneratedId.startsWith('30')).to.be.true
+
         expect(generatedId.startsWith('30I')).to.be.true
+        expect(anotherGeneratedId.startsWith('30I')).to.be.true
 
         const end1 = generatedId.replace(/^[0-9a-zA-Z]{18}/, '');
         const end2 = anotherGeneratedId.replace(/^[0-9a-zA-Z]{18}/, '');
