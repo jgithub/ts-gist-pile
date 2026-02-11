6a49a7755bccf70a9eb720cd42f015418832f254
JV <jgithub@users.noreply.github.com>
2026-02-11 12:37:41 -0600

    Add eventDate field to Happening, fix make dist target

--- dist/happening/Happening.d.ts
+++ dist/happening/Happening.d.ts
@@ -30,6 +30,7 @@ export interface Happening {
     sessionId?: string;
     uniqueVisitorToken?: string;
     ingestedAt?: Date;
+    eventDate?: number;
     serverName?: string;
     statusCode?: string;
     statusMessage?: string;
